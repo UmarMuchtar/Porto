3a4c700b9223ee6f22e3ea33dd7373d16cce5f55
Graell29 <umuchtar0@gmail.com>
2024-11-15 02:55:56 +0700

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['.public/index.html'],
+  content: [
+    "./public/index.html",
+  ],
   theme: {
-    extend: {
-      letterSpacing:{
-        viktor:'.5em'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
-
